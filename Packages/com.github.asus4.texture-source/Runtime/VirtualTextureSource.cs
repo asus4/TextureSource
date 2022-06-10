2f9e5a45c7da42b2611dd6cd7f00124de4895778
Koki Ibukuro <koki.ibukuro@gmail.com>
2022-06-10 16:01:14 +0200

    Add options to trim TextureSource to screen aspect

--- Packages/com.github.asus4.texture-source/Runtime/VirtualTextureSource.cs
+++ Packages/com.github.asus4.texture-source/Runtime/VirtualTextureSource.cs
@@ -22,13 +22,18 @@ namespace TextureSource
         [Tooltip("A texture source scriptable object for Editor. If it is null, used source in Editor")]
         private BaseTextureSource sourceForEditor = null;
 
+        [SerializeField]
+        private bool trimToScreenAspect = false;
+
         [Tooltip("Event called when texture updated")]
         public TextureEvent OnTexture = new TextureEvent();
+
         [Tooltip("Event called when the aspect ratio changed")]
         public AspectChangeEvent OnAspectChange = new AspectChangeEvent();
 
         private ITextureSource activeSource;
         private float aspect = float.NegativeInfinity;
+        private TextureTransformer transformer;
 
         public bool DidUpdateThisFrame => activeSource.DidUpdateThisFrame;
         public Texture Texture => activeSource.Texture;
@@ -51,6 +56,8 @@ namespace TextureSource
         private void OnDisable()
         {
             activeSource?.Stop();
+            transformer?.Dispose();
+            transformer = null;
         }
 
         private void Update()
@@ -60,7 +67,9 @@ namespace TextureSource
                 return;
             }
 
-            Texture tex = Texture;
+            Texture tex = trimToScreenAspect
+                ? TrimToScreen(Texture)
+                : Texture;
             OnTexture?.Invoke(tex);
 
             float aspect = (float)tex.width / tex.height;
@@ -76,6 +85,41 @@ namespace TextureSource
         public void NextSource()
         {
             activeSource?.Next();
+        }
+
+        private RenderTexture TrimToScreen(Texture texture)
+        {
+            float cameraAspect = (float)texture.width / texture.height;
+            float targetAspect = (float)Screen.width / Screen.height;
+
+            int width, height;
+            Vector2 scale;
+            if (cameraAspect > targetAspect)
+            {
+                width = RoundToEven(texture.height * targetAspect);
+                height = texture.height;
+                scale = new Vector2((float)texture.width / width, 1);
+            }
+            else
+            {
+                width = texture.width;
+                height = RoundToEven(texture.width / targetAspect);
+                scale = new Vector2(1, (float)texture.height / height);
+            }
+
+            bool needInitialize = transformer == null || width != transformer.width || height != transformer.height;
+            if (needInitialize)
+            {
+                transformer?.Dispose();
+                transformer = new TextureTransformer(width, height);
+            }
+
+            return transformer.Transform(texture, Vector2.zero, 0, scale);
+        }
+
+        private static int RoundToEven(float n)
+        {
+            return Mathf.RoundToInt(n / 2) * 2;
         }
     }
 }
