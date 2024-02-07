425170db2ba75915948fee1b86f3190327d8db2a
Koki Ibukuro <koki.ibukuro@gmail.com>
2024-02-07 16:21:02 +0100

    Split file

---
+++ Packages/com.github.asus4.texture-source/Resources/com.github.asus4.texture-source/Common.hlsl
@@ -0,0 +1,8 @@
+// Ported from RenderPipeLine-Core, to support both URP and Built-in RP in one shader
+// com.unity.render-pipelines.core copyright © 2020 Unity Technologies ApS
+// Licensed under the Unity Companion License for Unity-dependent projects--see [Unity Companion License](http://www.unity3d.com/legal/licenses/Unity_Companion_License).
+
+float3 FastSRGBToLinear(float3 c)
+{
+    return c * (c * (c * 0.305306011 + 0.682171111) + 0.012522878);
+}
